3d777c39af9eabb54a2f0fe10a36e97488e0dd0c
Asio0tus <o.o.bookkeeper.o.o@gmail.com>
2023-02-28 01:53:39 +0300

    new commit

--- Camera/CarCameraController.cs
+++ Camera/CarCameraController.cs
@@ -7,12 +7,47 @@ public class CarCameraController : MonoBehaviour
     [SerializeField] private Car car;
     [SerializeField] private new Camera camera;
     
-    [SerializeField] private CarCameraFollow cameraFollow;
-    [SerializeField] private CarCameraFovCorrector fovCorrector;    
+    [SerializeField] private CarCameraFollow carFollower;
+    [SerializeField] private CameraPathFollower pathFollower;
+    [SerializeField] private CarCameraFovCorrector fovCorrector;
+
+    [SerializeField] private RaceStateTracker raceStateTracker;
 
     private void Awake()
     {
-        cameraFollow.SetProperties(car, camera);
+        carFollower.SetProperties(car, camera);
         fovCorrector.SetProperties(car, camera);
     }
+
+    private void Start()
+    {
+        raceStateTracker.PeparationStarted += OnPeparationStarted;
+        raceStateTracker.Completed += OnCompleted;
+
+        carFollower.enabled = false;
+        pathFollower.enabled = true;
+    }
+
+    private void OnDestroy()
+    {
+        raceStateTracker.PeparationStarted -= OnPeparationStarted;
+        raceStateTracker.Completed -= OnCompleted;
+    }
+
+    private void OnPeparationStarted()
+    {
+        carFollower.enabled = true;
+        pathFollower.enabled = false;
+    }
+
+    private void OnCompleted()
+    {
+        pathFollower.enabled = true;
+        pathFollower.StartMoveToNearestPoint();
+        pathFollower.SetLookTarget(car.transform);
+
+        carFollower.enabled = false;
+    }
+
+
 }
